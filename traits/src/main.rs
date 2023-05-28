a320cc9d9328957b792834f214d0e4153ee443f6
leon0x01 <ushrestha358@gmail.com>
2023-05-28 11:26:56 +0545

    another example

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -1,4 +1,4 @@
-pub struct NewsArticle {
+/* pub struct NewsArticle {
     pub headline: String,
     pub location: String,
     pub author: String,
@@ -45,4 +45,98 @@ fn main() {
 
     println!("Tweet summary:{}", tweet.summarize());
     println!("Article summary:{}", article.summarize());
+} */
+
+/* struct Person {
+    citizenship: String,
+    name: String,
+    age: u8,
+    gender: char,
+    salary: i32,
+}
+
+struct Student {
+    name_std: String,
+    age: u8,
+    sex: char,
+    country: String,
+}
+
+trait General_info{
+    fn info(&self) -> (&str, u8, char); // just function signature not full implementation
+
+    fn country_info(&self) -> &str;
+}
+
+impl General_info for Person {
+    fn info(&self) -> (&str, u8, char){
+        (&(self.name), (self.age), self.gender)
+    }
+
+    fn country_info(&self) -> &str{
+        &self.citizenship
+    }
+}
+
+impl General_info for Student {
+    fn info(&self) -> (&str, u8, char) {
+        ((&self.name_std), (self.age), self.sex)
+    }
+    
+    fn country_info(&self) -> &str{
+        &self.country
+    }
+}
+
+fn main(){
+
+    let Person1 = Person{
+        name:String::from("daleon"),
+        citizenship: String::from("Nepal"),
+        age: 19,
+        gender: 'M',
+        salary: 40_000,
+    };
+
+    let student1 = Student{ 
+        name_std: String::from("Ram"),
+        age: 20, 
+        sex: 'M',
+        country: String::from("Nepal"),
+    };
+
+    println!("The basic info of the person {:?}", Person1.info()); 
+    println!("the country info of the person {:?}", Person1.country_info());
+}
+*/
+
+struct Circle {
+    radius: f32,
+}
+
+struct Rectangle {
+    length: f32,
+    width: f32,
+}
+
+trait Calculate {
+    fn area(&self) -> f32; 
+    fn perimeter(&self) -> f32; 
+}
+
+impl Calculate for Circle {
+    fn area(&self) -> f32 {
+        3.14*(&self.radius)*(&self.radius)
+    }
+
+    fn perimeter(&self) -> f32 {
+        2.0*3.14*(self.radius)
+    }
+}
+
+fn main(){
+    let a1 = Circle{
+        radius: 32.34,
+    };
+    println!("the area of the Circle is{:?}", a1.area());
 }
