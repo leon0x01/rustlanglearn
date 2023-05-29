71ca48862cded77530a39d7a7040c58308d2447e
leon0x01 <ushrestha358@gmail.com>
2023-05-29 08:19:18 +0545

    option<>

--- enums_part/src/main.rs
+++ enums_part/src/main.rs
@@ -70,15 +70,73 @@ fn main() {
 }
 */
 
-fn main() {
-    let five = Some(5);
-    let six  = plus_one(five);
-    let none = plus_one(None);
+// fn main() {
+//     let five = Some(5);
+//     let six  = plus_one(five);
+//     let none = plus_one(None);
+// }
+
+// fn plus_one(x:Option<i32>) -> Option<i32> {
+//     match x  {
+//         _ => None,
+//         Some(i) => Some(i+1),
+//     }
+// }
+/* enum Conveyance {
+    Car = 15,
+    Train = 20, 
+    Air = 30,
 }
 
-fn plus_one(x:Option<i32>) -> Option<i32> {
-    match x  {
-        _ => None,
-        Some(i) => Some(i+1),
+impl Conveyance {
+    fn travel_allowance(&self, miles:i32) -> f32 {
+       let allowance =   match self {
+            Conveyance::Car => miles as f32 * 14.0 * 2.0,
+            Conveyance::Train => miles as f32 * 18.0 * 2.0,
+            Conveyance::Air => miles as f32 * 34.0 * 2.0,
+
+        };
+        allowance
+    }
+}
+
+fn main() {
+    let participant_1 = Conveyance::Car;
+    // println!("The value of the option is {}", participant_1 as i32);
+    
+    println!("The allowance of the participant 1 is {:?}", participant_1.travel_allowance(10));
+}
+
+*/
+// #[derive(Debug)]
+// enum Value{
+//     Integer(i32),
+//     Float(f32),
+// }
+
+// fn main() {
+//     let some_value = vec![Value::Integer(32), Value::Float(1.0)];
+
+//     println!("the value is {:?}", some_value[0]);
+
+
+//     for i in some_value.iter(){
+//         match i {
+//             Value::Integer(num) => println!("The value is an interger type {}", num),
+//             Value::Float(num) => println!("The value is an float type {}", num ),
+//         }
+//     }
+// }
+
+//  =====================================================
+
+
+fn main(){
+    let  disease: Option<String> = None;
+    //disease = Some(String::from("Diabetes"));
+
+    match disease {
+        Some(disease_name) => println!("The disease is {}", disease_name),
+        None => println!("You do not have disease"),    
     }
 }
