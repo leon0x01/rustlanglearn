b26208ea2b60fb50dabc303e4bf6739cbe878583
leon0x01 <ushrestha358@gmail.com>
2023-05-31 14:17:56 +0545

    use external crates

--- Modules/src/main.rs
+++ Modules/src/main.rs
@@ -18,8 +18,23 @@
 //     file_2::some_person();
 // }
 
-mod file_3;
+// mod file_3;
 
-fn main() {
-    file_3::allowance();
+// fn main() {
+//     file_3::allowance();
+// }
+
+
+use array_tool::vec::*;
+
+fn main(){
+    let vec1 = vec![1,1,3,5,6,7];
+    let vec2 = vec![1,2,3];
+    let intersection = vec1.intersect(vec2.clone());
+    println!("The intersection  = {:?}", intersection);
+
+    let union = vec1.union(vec2.clone());
+    println!("The union = {:?}", union);
+
+    println!("vec 1 three times displayed = {:?}", vec2.times(3));
 }
